fd6301f5a90e5d781b03f7a6c23cf0f9d7ea6380
nisarga-nn <nisargan2000@gmail.com>
2023-05-10 17:58:46 +0530

    dashboad added

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,34 @@
-import {createRouter,createWebHistory} from 'vue-router'
-import About from '@/views/About.vue'
-import Home from '@/views/Home.vue'
-import Contact from '@/views/Contact.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import About from "@/views/About.vue";
+import Home from "@/views/Home.vue";
+import Contact from "@/views/Contact.vue";
+import Dashboard from "@/views/Dashboard.vue"
 
 const router = createRouter({
-    // history: createWebHistory(import.meta.env.BASE_URL),
-    history: createWebHistory(),
-    routes:[
-        {
-            path:"/",
-            name:"home",
-            component: Home
-        },
-        {
-            path:"/about",
-            name:"about",
-            component: About
-        }, {
-            path:"/contact",
-            name:"contact",
-            component: Contact
-        }
-    ]
-})
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: Contact,
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+    },
+  ],
+});
 
 export default router;
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<script>
+export default  {
+    name : 'DashboardPage'
+}
+</script>
+
+<template>
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+</template>
